561bdaf4672fbe79007cc7d621129fe35839bc88
Theo Richart <theo.richart@gmail.com>
2021-03-30 15:51:31 -0400

    wip

--- Assets/Scripts/Systems/ItemSpawningSystem.cs
+++ Assets/Scripts/Systems/ItemSpawningSystem.cs
@@ -1,45 +1,64 @@
 using System;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Rendering;
 using UnityEngine;
 
 namespace Automation
 {
+    // class ItemRenderingSystem : SystemBase
+    // {
+    //     private InstancedRenderMeshBatchGroup _group;
+    //
+    //     protected override void OnCreate()
+    //     {
+    //         base.OnCreate();
+    //         _group = new Unity.Rendering.InstancedRenderMeshBatchGroup(EntityManager, this, default);
+    //     }
+    //
+    //     protected override void OnUpdate()
+    //     {
+    //         _group.
+    //     }
+    // }
+
     class ItemSpawningSystem : SystemBase
     {
         private ItemSpawningCommandSystem _ecbSystem;
-
-        protected override void OnCreate()
-        {
-            _ecbSystem = World.GetExistingSystem<ItemSpawningCommandSystem>();
-        }
+        private EntityQuery _getEntityQuery;
 
         public struct SpawnedItemVisual : IComponentData
         {
             public Entity BeltSegment;
             public int BeltItemIndex;
         }
-        
+
+        protected override void OnCreate()
+        {
+            _ecbSystem = World.GetExistingSystem<ItemSpawningCommandSystem>();
+            _getEntityQuery = GetEntityQuery(ComponentType.ReadWrite<SpawnedItemVisual>());
+        }
+
         protected override void OnUpdate()
         {
             // var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp, PlaybackPolicy.MultiPlayback);
             
             var settings = GetSingleton<World.Settings>();
+            var itemBuffers = GetBufferFromEntity<BeltItem>();
             Entities.ForEach((Entity e, in SpawnedItemVisual v) =>
             {
                 
-                var b = EntityManager.GetBuffer<BeltItem>(v.BeltSegment);
+                var b = itemBuffers[v.BeltSegment];
                 b.ElementAt(v.BeltItemIndex).Entity = e;
-                // entityCommandBuffer.RemoveComponent<SpawnedItemVisual>(e);
-            }).WithoutBurst().Run();
-            // entityCommandBuffer.Playback(EntityManager);
-            
-            EntityManager.RemoveComponent<SpawnedItemVisual>(GetEntityQuery(ComponentType.ReadWrite<SpawnedItemVisual>()));
+            }).Run();
+
+            EntityManager.RemoveComponent<SpawnedItemVisual>(_getEntityQuery);
             
             var prefab = GetSingleton<World.Prefabs>();
 
             var ecb = _ecbSystem.CreateCommandBuffer().AsParallelWriter();
-            _ecbSystem.AddJobHandleForProducer(Dependency = Entities
+            _ecbSystem.AddJobHandleForProducer(
+                Dependency = Entities
                 .ForEach((Entity e, int entityInQueryIndex, DynamicBuffer<BeltItem> items, in BeltSegment segment) =>
                 {
                     float dist = 0;
@@ -60,7 +79,7 @@ namespace Automation
                             {
                                 BeltSegment = e,BeltItemIndex = i,
                             });
-
+                       
                             ecb.AddComponent(entityInQueryIndex, itemEntity, beltItemVisual);
                         }
                         else
@@ -68,7 +87,9 @@ namespace Automation
 
                     }
                 })
-                .ScheduleParallel(Dependency));
+                .ScheduleParallel(Dependency)
+                    )
+                    ;
         }
     }
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -41,53 +41,55 @@ namespace Automation
             });
             
             // MakeT(dstManager, prefabEntity);
-            Make3BeltsU(dstManager, prefabEntity, 1000000);
+            Make3BeltsU(dstManager, prefabEntity, 2, 5);
         }
 
         private void MakeT(EntityManager dstManager, Entity prefabEntity)
         {
             var entities = dstManager.Instantiate(prefabEntity, 2, Allocator.Temp);
-            CreateSegment(dstManager, new BeltSegment
+            CreateSegment(dstManager, entities[0],
+                new BeltSegment
                 {
                     Start = new int2(3, 5),
                     End = new int2(12, 5),
                     Next = entities[1],
-                }, entities[0],
-                (EntityType.A, 1),(EntityType.A, 4)
-            );
-            CreateSegment(dstManager, new BeltSegment
+                },(EntityType.A, 1), (EntityType.A, 4));
+            CreateSegment(dstManager, entities[1],
+                new BeltSegment
                 {
                     Start = new int2(13, 2),
                     End = new int2(13, 10),
-                }, entities[1],
-                (EntityType.B, 8));
+                }, (EntityType.B, 8));
             entities.Dispose();
         }
 
-        private void Make3BeltsU(EntityManager dstManager, Entity prefabEntity, int itemCount = 2)
+        private void Make3BeltsU(EntityManager dstManager, Entity prefabEntity, int itemCount = 2, int beltCount = 2)
         {
-            var entities = dstManager.Instantiate(prefabEntity, 3, Allocator.Temp);
-            CreateSegment(dstManager, new BeltSegment
-                {
-                    Start = new int2(-itemCount, 5),
-                    End = new int2(12, 5),
-                    Next = entities[1],
-                }, entities[0],
-                Enumerable.Range(0, itemCount).Select(x => (x % 2 == 0 ? EntityType.A : EntityType.B, (ushort) 1)).ToArray()
-// (EntityType.A, 1),(EntityType.B, 1)
-            );
-            CreateSegment(dstManager, new BeltSegment
+            var entities = dstManager.Instantiate(prefabEntity, 2 + beltCount, Allocator.Temp);
+            Entity first = entities[0];
+            for (int i = 0; i < beltCount; i++)
             {
-                Start = new int2(13, 5),
-                End = new int2(13, 10),
-                Next = entities[2],
-            }, entities[1]);
-            CreateSegment(dstManager, new BeltSegment
+                CreateSegment(dstManager, entities[2+i],
+                    new BeltSegment
+                    {
+                        Start = new int2(-5*(beltCount-i)-1, 5),
+                        End = new int2(-5*(beltCount-i-1), 5),
+                        Next = i < beltCount - 1 ? entities[2+i+1] : first,
+                    }, /*i != 0 ? null :*/ Enumerable.Range(0, itemCount).Select(x => (x % 2 == 0 ? EntityType.A : EntityType.B, (ushort) 2)).ToArray()
+                );
+            }
+            CreateSegment(dstManager, entities[0], new BeltSegment
             {
-                Start = new int2(13, 11),
+                Start = new int2(1, 5),
+                End = new int2(1, 10),
+                Next = entities[1],
+            });
+            CreateSegment(dstManager, entities[1], new BeltSegment
+            {
+                Start = new int2(1, 11),
                 End = new int2(4, 11),
                 // Next = entities[3],
-            }, entities[2]);
+            });
             // CreateSegment(dstManager, new BeltSegment
             // {
             //     Start = new int2(3,11),
@@ -97,7 +99,7 @@ namespace Automation
             entities.Dispose();
         }
 
-        private void CreateSegment(EntityManager dstManager, BeltSegment segment, Entity beltSegmentEntity,
+        private void CreateSegment(EntityManager dstManager, Entity beltSegmentEntity, BeltSegment segment,
             params (EntityType, ushort)[] beltItems)
         {
             dstManager.AddComponentData(beltSegmentEntity, segment);
@@ -109,9 +111,10 @@ namespace Automation
                     (segment.End.x + segment.Start.x) / 2f, -.05f, (segment.End.y + segment.Start.y) / 2f)
             });
             var items = dstManager.AddBuffer<BeltItem>(beltSegmentEntity);
-            foreach (var beltItem in beltItems)
-                items.Add(new BeltItem(beltItem.Item1, (ushort) (beltItem.Item2*SubdivCount)));
-            dstManager.SetName(beltSegmentEntity, segment.ToString());
+            if(beltItems != null)
+                foreach (var beltItem in beltItems)
+                    items.Add(new BeltItem(beltItem.Item1, (ushort) (beltItem.Item2*SubdivCount)));
+            dstManager.SetName(beltSegmentEntity, "segment");
 
             // RenderMeshUtility.AddComponents(beltSegmentEntity, dstManager, new RenderMeshDescription(Prefab.GetComponent<Renderer>(), Prefab.GetComponent<MeshFilter>().sharedMesh));
             var size = new float3(length+1, .1f, 1);
