9ce1216f72e23b0bf82ba8d97ab51e5c6baca9d3
Theo Richart <theo.richart@gmail.com>
2021-04-02 21:20:50 -0400

    wip splitter

--- Assets/Scripts/Components/BeltSegment.cs
+++ Assets/Scripts/Components/BeltSegment.cs
@@ -7,8 +7,15 @@ using UnityEngine;
 
 namespace Automation
 {
+    struct BeltMerger : IComponentData
+    {
+        
+    }
+    struct BeltSplitter : IComponentData{}
     struct BeltSegment : IComponentData
     {
+        public static float3 FromI2(int2 i2) => new float3(i2.x, 0, i2.y);
+        
         public int2 Start;
         public int2 End;
         public Entity Next;
@@ -20,10 +27,7 @@ namespace Automation
 
         [JsonIgnore] public readonly int2 DropPoint => End + Dir;
 
-        public static float3 FromI2(int2 i2)
-        {
-            return new float3(i2.x, 0, i2.y);
-        }
+
         public readonly AABB AABB =>
             new AABB
             {
@@ -31,66 +35,16 @@ namespace Automation
                 Extents = 0.5f * (FromI2(math.abs(End - Start))) + 1,
             };
 
-        public override string ToString() => $"Segment {Start} -> {End}";
-
-        public void InsertItem(in World.Settings settings, ref DynamicBuffer<BeltItem> items, BeltItem segmentItem,
-            int2 dropPoint)
-        {
-            segmentItem.Distance = 0;
-            var targetDist = math.abs(dropPoint.x - DropPoint.x + dropPoint.y - DropPoint.y) * settings.BeltDistanceSubDiv;
-            int itemIdx = 0;
-            int iter = items.Length + 2;
-            var dist = 0;
-            // Debug.Log($"Insert start in {Start} -> {End} target {dropPoint} dir {Dir} revdir {RevDir} targetDist {targetDist}");
-            while (dist != targetDist)
-            {
-                if(iter-- <= 0)
-                    throw new NotImplementedException();
-                var delta = (ushort) (targetDist - dist);
-                if (itemIdx < items.Length)
-                {
-                    ref var item = ref items.ElementAt(itemIdx);
-                    if (item.Distance + dist < targetDist)
-                    {
-                        dist += item.Distance;
-                        itemIdx++;
-                    }
-                    else
-                    {
-                        segmentItem.Distance = delta;
-                        item.Distance -= segmentItem.Distance;
-                        break;
-                    }
-                }
-                else
-                {
-                    segmentItem.Distance = delta;
-                    break;
-                }
-            }
-        
-            // if (itemWillBeTickedAgain)
-            //     segmentItem.Distance++;
-            // if not last item, patch next one
-            // if (itemIdx < Items.Count)
-            // {
-            //     var i = Items[itemIdx];
-            //     i.Distance = (byte) (i.Distance - segmentItem.Distance - 1);
-            //     Items[itemIdx] = i;
-            // }
-            items.Insert(itemIdx, segmentItem);
-        }
-
         public void ComputeInsertionPoint(ref DynamicBuffer<BeltItem> items, ushort subdivCount)
         {
             var acc = 0;
             for (int i = 0; i < items.Length; i++)
-            {
                 acc += items[i].Distance;
-            }
 
             var length = math.abs(DropPoint-Start) * subdivCount;
-            this.DistanceToInsertAtStart = (ushort) (length.x + length.y - acc);
+            DistanceToInsertAtStart = (ushort) (length.x + length.y - acc);
         }
+
+        public override string ToString() => $"Segment {Start} -> {End}";
     }
 }
--- Assets/Scripts/Systems/BeltUpdateSystem.cs
+++ Assets/Scripts/Systems/BeltUpdateSystem.cs
@@ -151,28 +151,4 @@ namespace Automation
                     }).Run();
         }
     }
-    
-    public static class HaltonSequence
-    {
-        public static double Halton(int index, int nbase)
-        {
-            double fraction = 1;
-            double result = 0;
-            while (index > 0)
-            {
-                fraction /= nbase;
-                result += fraction * (index % nbase);
-                index = ~~(index / nbase);
-            }
-
-            return result;
-        }
-
-        // shortcut for later
-        public static int HaltonInt(int index, int nbase, int max) => (int) (Halton(index, nbase) * max);
-        public static Color ColorFromIndex(int index, int hbase = 3, float v = 0.5f)
-        {
-            return Color.HSVToRGB((float) HaltonSequence.Halton(index, hbase), 1, v);
-        }
-    }
 }
